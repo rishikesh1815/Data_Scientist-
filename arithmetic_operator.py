ec9eb78ae9ef9259282ff12ac221bab362dbe0d2
rishisharu <rishikesh1815@gmail.com>
2025-12-10 12:09:22 +0530

    new python file added

---
+++ arithmetic_operator.py
@@ -0,0 +1,12 @@
+# python has more set of operstors.In that arithmetic operator is there.
+# Arithmetic operator is used to perform mathematical operations to answer for complex problems easily.
+# we can also perform arithmetic operations by get the values as input.
+
+num1 = input("enter first number: ")
+num2 = input("enter second number: ")
+
+sum = num1 + num2
+sub = num1 - num2
+multiply = num1 * num2
+div = num1 / num2
+remainder = num1 % num2
